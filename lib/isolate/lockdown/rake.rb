0ce8b67a38862aca21dd4afea1b2ec27c6ad07d8
Luis Lavena <luislavena@gmail.com>
2010-12-24 20:21:13 -0300

    Copy gem contents into folder

--- lib/isolate/lockdown/rake.rb
+++ lib/isolate/lockdown/rake.rb
@@ -9,7 +9,7 @@ namespace :isolate do
     temp_folder = "#{folder}.#{$$}"
 
     # remove previous safe folder if exists
-    when_writing "remove previous safe folder" do
+    when_writing "Removing previous safe folder" do
       FileUtils.rm_rf safe_folder if File.exist?(safe_folder)
     end
 
@@ -23,7 +23,7 @@ namespace :isolate do
     require_paths = []
 
     specs.each do |spec|
-      puts "Locking down #{spec.name} version #{spec.version}..."
+      puts "Locking down #{spec.name} version #{spec.version}"
 
       base = spec.full_gem_path
 
@@ -34,6 +34,29 @@ namespace :isolate do
         FileUtils.mkdir_p(bin_dir) unless File.exist?(bin_dir)
         FileUtils.cp full_exe, bin_dir
       end
+
+      spec.require_paths.each do |path|
+        # do not add bindir to load path
+        next if path == spec.bindir
+
+        lib_dir = File.join(temp_folder, path)
+        require_paths << File.join(folder, path)
+
+        FileUtils.mkdir_p(lib_dir)
+
+        glob = File.join(base, path, '*')
+        Dir.glob(glob).each do |full_lib|
+          if File.file?(full_lib)
+            FileUtils.cp(full_lib, lib_dir)
+          else
+            FileUtils.cp_r(full_lib, lib_dir)
+          end
+        end
+      end
+    end
+
+    FileUtils.mv(folder, safe_folder) if File.exist?(folder)
+    FileUtils.mv(temp_folder, folder)
     end
   end
 end
